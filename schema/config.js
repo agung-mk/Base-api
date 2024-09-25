b9c2899effdac4e2679a4707a692ca37ae7d002b
siagungg_ <147239620+agung-mk@users.noreply.github.com>
2024-09-25 13:30:47 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -11,7 +11,7 @@ const options = {
 module.exports = {
   options,
   host: {
-    BASE_URL: 'domain.xyz' 
+    BASE_URL: 'agungdev.us.kg' 
 /*
 Example: domain.com
 don't use https, just the domain name!
